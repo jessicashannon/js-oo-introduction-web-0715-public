7bae1e4977c8bd81b9e17a71b3f8c0896455c307
jessicashannon <jessdahling@gmail.com>
2015-09-11 08:50:34 -0400

    done

--- js/bike.js
+++ js/bike.js
@@ -1,7 +1,20 @@
-function Bike(){
-  // attributes go here...
+function Bike(name, type, color){
+  this.name = name;
+  this.type = type;
+  this.color = color;
+  this.frameColor = color;
+  this.bikeType = type;
+  this.condition = 'ready to go!';
+  this.WHEELS = 2;
 }
 
-Bike.prototype.someInstanceMethod = function(){
-  // code goes here...
-};
+ Bike.prototype.takeForARide = function(miles){
+   var that = this;
+   if(miles>50){
+     that.condition = 'needs a tune up';
+   }
+ };
+
+ Bike.prototype.tuneUp = function(){
+   this.condition = 'ready to go!';
+ }
--- js/owner.js
+++ js/owner.js
@@ -1,7 +1,27 @@
-function Owner(){
-  // attributes go here...
+function Owner(name){
+  this.name = name;
+  this.bikes = [];
+  this.currentbike = ""
 }
 
-Owner.prototype.someInstanceMethod = function(){
-  // code goes here...
-};
+Owner.prototype.buildBike = function (bike, type, color){
+  var bike = new Bike(bike, type, color);
+  this.bikes.push(bike);
+  };
+
+Owner.prototype.rideBike = function rideBike(bikename, distance){
+  var that = this
+  this.bikes.forEach(function(bike){
+    if(bike.name == bikename){
+      that.currentbike = bike;
+      };
+    });
+  if (distance > 50){
+    this.currentbike.condition = "needs a tune up";
+    };
+  };
+
+
+// Owner.prototype.someInstanceMethod = function(){
+//   // code goes here...
+// };
